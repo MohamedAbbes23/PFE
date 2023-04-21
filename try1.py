d01e6067df3fe8a990701ae55dfa71d20d857476
MohamedAbbes23 <121729769+MohamedAbbes23@users.noreply.github.com>
2023-04-21 20:56:54 +0100

    Update try1.py

--- try1.py
+++ try1.py
@@ -1,1 +1,24 @@
-print('hey')
+import numpy as np
+from pylfsr import LFSR
+
+state = [0,0,0,1,0]
+fpoly = [5,3]
+L = LFSR(fpoly=fpoly,initstate =state)
+
+# Generate K-bits
+k=10
+seq_k  = L.runKCycle(k)
+
+print('10 bits')
+print(L.arr2str(seq_k))
+print('')
+
+
+# Generate bits of one full period.
+# Expected period of LFSR = 2^M-1, for M-bit LFSR).
+seq_full  = L.runFullPeriod()
+
+print('Full period of LFSR = 31-cycles')
+print(L.arr2str(seq_full))
+
+L.Viz()
