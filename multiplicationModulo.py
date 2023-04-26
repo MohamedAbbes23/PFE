e21b331d1b9d631893a74e63f0d43752c158f3a4
MohamedAbbes23 <121729769+MohamedAbbes23@users.noreply.github.com>
2023-04-26 18:59:35 +0100

    Update multiplicationModulo.py

--- multiplicationModulo.py
+++ multiplicationModulo.py
@@ -30,5 +30,12 @@ b = 65746311545646431;
 m = 10005412336548794;
 print(moduloMultiplication(a, b, m));
 	
-# This code is contributed
-# by Shivi_Aggarwal
+# Import math Library
+import math
+
+# Return the sine of different values
+print (math.sin(0.00))
+print (math.sin(-1.23))
+print (math.sin(10))
+print (math.sin(math.pi))
+print (math.sin(math.pi/2))
