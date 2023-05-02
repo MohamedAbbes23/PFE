7f21b3f8d93d48b9798e02053b43fa195345cd07
MohamedAbbes23 <121729769+MohamedAbbes23@users.noreply.github.com>
2023-05-02 20:35:00 +0100

    Update toAscii.py

--- toAscii.py
+++ toAscii.py
@@ -33,6 +33,15 @@ for character in text:
 print(ascii_values)
 #********************************************
 num = 24.89
+# Initializing two integers a and b
+a = 15
+b = 32
+
+# Finding XOR of a and b using ^ operator
+c = a ^ b
+
+# Printing the XOR value
+print(a, "XOR", b, "=", c)
 
 rounded = round(num, 1)
 print(rounded)
