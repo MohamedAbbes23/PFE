8a3b6f4bf9c00fa703de2027395be41f5837d93d
MohamedAbbes23 <121729769+MohamedAbbes23@users.noreply.github.com>
2023-05-05 19:47:17 +0100

    Update try1.py

--- try1.py
+++ try1.py
@@ -29,3 +29,15 @@ import math
 print (math.pi)
 #*************************************************
 xor_num = num1 ^ num2
+
+
+
+import hashlib
+ 
+# encoding GeeksforGeeks using md5 hash
+# function
+result = hashlib.md5(b'GeeksforGeeks')
+ 
+# printing the equivalent byte value.
+print("The byte equivalent of hash is : ", end ="")
+print(result.digest())
