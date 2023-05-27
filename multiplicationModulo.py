b2c9f879fcc93bc5f4552c2dd20032a45f490a7f
MohamedAbbes23 <121729769+MohamedAbbes23@users.noreply.github.com>
2023-05-27 22:08:51 +0100

    Update multiplicationModulo.py

--- multiplicationModulo.py
+++ multiplicationModulo.py
@@ -1,19 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>YouTube Downloader</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-</head>
-<body>
-  <div class="container">
-    <h1 class="mt-4">YouTube Downloader</h1>
-    <form class="mt-4">
-      <div class="form-group">
-        <label for="videoUrl">Enter YouTube Video URL:</label>
-        <input type="text" class="form-control" id="videoUrl" placeholder="https://www.youtube.com/watch?v=VIDEO_ID">
-      </div>
-      <button type="submit" class="btn btn-primary">Download</button>
-    </form>
-  </div>
-</body>
-</html>
+from mpmath import mp
+
+# Set the desired precision (number of decimal places)
+precision = 9999
+
+# Set the working precision of mpmath
+mp.dps = precision
+
+# Calculate Pi with the desired precision
+pi = mp.pi
+
+# Print Pi with the specified precision
+print(f"Pi: {pi}")
