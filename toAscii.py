9a47d19ee1feeccf977b1b61fdcf3e6b22418206
MohamedAbbes23 <121729769+MohamedAbbes23@users.noreply.github.com>
2023-05-28 20:20:28 +0100

    Update toAscii.py

--- toAscii.py
+++ toAscii.py
@@ -1,47 +1,14 @@
-# Python3 code to demonstrate working of
-# Convert String list to ascii values
-# using loop + ord()
+from pytube import YouTube
+from sys import argv
 
-# initialize list
-test_list = ['gfg', 'is', 'best']
+link = argv[1]
+yt = YouTube(link)
 
-# printing original list
-print("The original list : " + str(test_list))
+print("Title: ", yt.title)
 
-# Convert String list to ascii values
-# using loop + ord()
-res = []
-for ele in test_list:
-	res.extend(ord(num) for num in ele)
+print("View: ", yt.views)
 
-# printing result
-print("The ascii list is : " + str(res))
-#***********************************
-a = 79
+yd = yt.streams.get_highest_resolution()
 
-# Base 2(binary)
-bin_a = bin(a)
-print(bin_a)
-print(int(bin_a, 2))
-
-
-#python 3.x
-text = input("enter a string to convert into ascii values:")
-ascii_values = []
-for character in text:
-    ascii_values.append(ord(character))
-print(ascii_values)
-#********************************************
-num = 24.89
-# Initializing two integers a and b
-a = 15
-b = 32
-
-# Finding XOR of a and b using ^ operator
-c = a ^ b
-
-# Printing the XOR value
-print(a, "XOR", b, "=", c)
-
-rounded = round(num, 1)
-print(rounded)
+# ADD FOLDER HERE
+yd.download('./YTfolder')
